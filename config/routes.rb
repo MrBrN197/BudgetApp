589b51dd5e192f227e4c865d119b1eabef9fee25
brianorora <brianorora@gmail.com>
2022-02-07 00:55:42 +0300

    Redirect to `categories#index` after sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
+  get '/user', to: 'categories#index', as: :user_root
+
   root 'splash#index'
 
   resources :categories, only: [:index, :show] do
