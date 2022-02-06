ef01b3b6d2a09f6e563f937871e018e3c6d4257a
brianorora <brianorora@gmail.com>
2022-02-06 20:41:31 +0300

    Create migrations for creating indexes
    - `AddRecordRefToCategories`
    - `AddCategoryRefToRecords`

--- db/migrate/20220206165228_create_records.rb
+++ db/migrate/20220206165228_create_records.rb
@@ -4,7 +4,6 @@ class CreateRecords < ActiveRecord::Migration[7.0]
       t.string :name
       t.float :ammount
       t.references :user, null: false, foreign_key: true
-      t.references :category, null: false, foreign_key: true
 
       t.timestamps
     end
