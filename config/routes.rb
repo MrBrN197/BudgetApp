17d26de6ef2bb897607ecd0035464fdcb9c2cc96
brianorora <brianorora@gmail.com>
2022-02-06 20:12:00 +0300

    Add routes for categories -> records

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
   # root "articles#index"
+
+  resources :categories, only: [:index, :show] do
+    resources :records, only: [:index, :show]
+  end
+
 end
