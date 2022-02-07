6192e00e332e7f975918f547f38e6ca29ca070d2
brianorora <brianorora@gmail.com>
2022-02-07 14:21:07 +0300

    Add validations for models

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,4 +2,6 @@ class Category < ApplicationRecord
   belongs_to :user
   has_many :categories_records, dependent: :destroy
   has_many :records, through: :categories_records
+
+  validates :name, presence: true
 end
--- app/models/record.rb
+++ app/models/record.rb
@@ -2,4 +2,7 @@ class Record < ApplicationRecord
   belongs_to :user
   has_many :categories_records
   has_many :categories, through: :categories_records
+
+  validates :name, presence: true
+  validates :ammount, numericality: { greater_than: 0 }
 end
