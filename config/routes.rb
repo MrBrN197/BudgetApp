0f5188e8da79d30d2d9843044a594b0e753acb6b
brianorora <brianorora@gmail.com>
2022-02-09 19:14:30 +0300

    Fix: redirect to categories page after sign_in

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  get '/user', to: 'categories#index', as: :user_root
 
   root 'splash#index'
 
   resources :categories do
     resources :records
   end
-
 end
